5536d801bd663ac4644e3fd1dc4bbb2a8d3310ff
Miraan Ejaz <miraanejaz1@gmail.com>
2026-08-26 14:22:40 +0000

    Python practice added

--- ask_ai.py
+++ ask_ai.py
@@ -1,3 +1,15 @@
 print("Hello, welcome to the Ask AI Project!")
 
-print(Hello)
+
+name = input("Enter your name: ")
+
+city = input("Enter your city: ")
+
+print(f"Hello {name}, nice to meet you. You live in {city}.")
+
+
+num = input("Enter your age: ")
+
+print(f"Your age is {num} and the type of {num} is {type(num)}")
+print(f"Next year you will be {int(num) + 1} years old")
+
